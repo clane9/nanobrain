96bd1c5cd5c182dedb5b1b2f2a6be852160405a7
Connor Lane <connor.lane858@gmail.com>
2026-09-20 20:47:52 +0000

    add image conform util
    similar to nibabel conform, but uses a minimum voxel size and max fov.

--- src/nanobrain/utils/preprocessing.py
+++ src/nanobrain/utils/preprocessing.py
@@ -1,7 +1,10 @@
+import math
+
 import nibabel as nib
 import numpy as np
 import scipy.ndimage as ndi
-from nibabel.processing import resample_to_output, resample_from_to
+from nibabel.affines import rescale_affine
+from nibabel.processing import resample_to_output, resample_from_to, smooth_image
 
 
 def threshold_mask(
@@ -60,3 +63,35 @@ def largest_component(mask: np.ndarray):
         sizes = ndi.sum_labels(mask, label, range(1, count + 1))
         mask = label == (np.argmax(sizes) + 1)
     return mask
+
+
+def conform_image(
+    img: nib.Nifti1Image,
+    min_voxel_size: float = 1.0,
+    max_fov: tuple[float, float, float] = (256.0, 256.0, 256.0),
+    order: int = 1,
+):
+    """Conform image to target minimum voxel size and max FOV.
+
+    References:
+        https://github.com/nipy/nibabel/blob/5.4.2/nibabel/processing.py#L318
+    """
+    assert img.ndim == 3, f"expected 3D image, got {img.ndim}"
+    img = nib.as_closest_canonical(img)
+
+    # new voxel size no smaller than min voxel size
+    voxel_sizes = img.header.get_zooms()
+    new_voxel_sizes = [max(min_voxel_size, sz) for sz in voxel_sizes]
+    # new fov no bigger than target
+    fov = [w * sz for w, sz in zip(img.shape, voxel_sizes)]
+    new_fov = [min(w_, w) for w_, w in zip(max_fov, fov)]
+    new_shape = [math.ceil(w / sz) for w, sz in zip(new_fov, new_voxel_sizes)]
+
+    # smooth if downsampling
+    if any(sz_ > sz for sz_, sz in zip(new_voxel_sizes, voxel_sizes)):
+        img = smooth_image(img, fwhm=new_voxel_sizes)
+
+    # resample with cropping
+    new_affine = rescale_affine(img.affine, img.shape, new_voxel_sizes, new_shape)
+    new_img = resample_from_to(img, (new_shape, new_affine), order=order)
+    return new_img
---
+++ src/nanobrain/utils/test_preprocessing.py
@@ -0,0 +1,37 @@
+import pytest
+import nibabel as nib
+import numpy as np
+import scipy.ndimage as ndi
+from nibabel.processing import resample_to_output
+from templateflow import api as tflow
+
+import nanobrain.utils.preprocessing as preproc
+
+
+@pytest.fixture(scope="module")
+def test_img() -> nib.Nifti1Image:
+    client = tflow.TemplateFlowClient()
+    path = client.get("MNI152NLin6Asym", desc=None, resolution=1, suffix="T1w", extension="nii.gz")
+    img = nib.load(path)
+    return img
+
+
+def test_threshold_mask(test_img: nib.Nifti1Image):
+    mask_img = preproc.threshold_mask(test_img)
+    assert mask_img.shape == test_img.shape
+    assert (mask_img.affine == test_img.affine).all()
+
+    mask = np.asarray(mask_img.dataobj) > 0
+    label, count = ndi.label(mask)
+    assert count == 1
+
+
+def test_conform_image(test_img: nib.Nifti1Image):
+    test_img = resample_to_output(test_img, (0.5, 1.0, 3.0), order=1)
+    mvs = 1.0
+    max_fov = (208.0, 240.0, 120.0)
+    fit_img = preproc.conform_image(test_img, min_voxel_size=mvs, max_fov=max_fov)
+    spacing = fit_img.header.get_zooms()
+    fov = [w * sz for w, sz in zip(fit_img.shape, spacing)]
+    assert all(sz >= mvs for sz in spacing)
+    assert all(w <= w_ for w, w_ in zip(fov, max_fov))
